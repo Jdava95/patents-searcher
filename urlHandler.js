b883f89eb5096e51637786dae2e724110aab3325
Javid Alimli <javidalimli@MacBook-Pro-Javid.local>
2019-08-20 16:26:02 +0300

    удалил не нужный кусок

--- urlHandler.js
+++ urlHandler.js
@@ -27,24 +27,11 @@ async function urlHandler() {
       const result = await parser.parse(options);
       console.log(`Успешно ${result.success} , не успешно ${result.fail}`)
 
-    res.on('end', () => {
-      console.log('done')
-    })
+      res.on('end', () => {
+        console.log('done')
+      })
   })
 }
 
-
-const fileHandler = async function (Model, { name, path }) {
-  try {
-    if (!Model) throw new Error(`Model ${name} is undefined`);
-    const rs = fs.createReadStream(path);
-    const parser = new CsvParser(rs, Model);
-    const options = parserOptions[name] || {};
-    const result = await parser.parse(options);
-    console.log(`Успешно ${result.success} , не успешно ${result.fail}`)
-  } catch (err) {
-    console.error(err);
-  }
-}
 
 module.exports = urlHandler;
