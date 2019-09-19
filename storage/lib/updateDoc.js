e9009e3e6f414d20882f39d3b5f4dd78553d6bd5
Javid Alimli <javidalimli@MacBook-Pro-Javid.local>
2019-09-19 12:19:31 +0300

    исправлено фарматирование, удален лишний комментарий

--- storage/lib/updateDoc.js
+++ storage/lib/updateDoc.js
@@ -2,13 +2,13 @@
  * принимает поток, добавляет или перезаписывает запись 
  * @param {Object} options 
  */
-function updateDoc ( options ) {
-  // const { registrationNumber } = options;
+function updateDoc(options) {
   return this.updateOne({
-    registrationNumber: options.registrationNumber}, 
+      registrationNumber: options.registrationNumber
+    },
     options, {
-    upsert: true
-  });
+      upsert: true
+    });
 }
 
 module.exports = updateDoc;
